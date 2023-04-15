88f2df1368e70d01f914f795b7ec29f624761b1a
maxai <maxai@MAX>
2023-04-15 22:49:21 +0800

    Add project files.

---
+++ introduction/Program.cs
@@ -0,0 +1,24 @@
+﻿string[] username = { "max", "glezel", "barben" };
+string[] password = { "xam", "power1", "power2" };
+
+Console.WriteLine("Login:");
+string login = Console.ReadLine();
+Console.WriteLine("password:");
+string passwords = Console.ReadLine();
+
+bool isFound = false;
+
+for (int i = 0; i < username.Length; i++)
+{
+    if (login == username[i] && passwords == password[i])
+    {
+        isFound = true;
+        Console.WriteLine("Login: " + username[i]);
+        break;
+    }
+}
+if (!isFound) Console.WriteLine("No account match found!");
+
+
+
+
